99cdbe44b2edeb12d4c774bfafd8d6122842d8cb
Bruno Luiz <contato@brunoluiz.net>
2015-10-29 21:19:21 +0000

    Added an IRQ alternative to the last question

---
+++ lab3-challenge-irqsys.py
@@ -0,0 +1,74 @@
+'''
+One tasks system:
+- ledDemoTask: demonstrate the LED depending on the user config
+
+Two interrupts:
+- buttonColourModeIRQ: checks the if the button for colourMode was pressed
+- buttonSpeedIRQ: checks the if the button for speed was pressed
+
+There is a global struct called Settings to make the inter-task communication possible.
+The two import proprieties of the struct are:
+- speed: save the choosen speed (based on the user config with the button)
+- colourMode: save the choosen colour mode (based on the user config with the button)
+- speedToogle and colourModeToogle: just toogle the value of the above properties
+'''
+
+# This example consider that will be some struct/class defining
+# some functions to check some states for buttonColour and buttonSpeed, as:
+# - button.isHolded()
+# - button.isPressed()
+import HWButtons
+import HWFunctions
+
+'''
+INTER-TASK COMMUNICATION STRUCT
+'''
+# Settings would save two state variables:
+# - speed: SLOW or FAST 
+# - colourMode: LED_PER_LED or MULTIPLE_LEDS
+# and the funcions to toogle it
+class Settings:
+    speed = LED_PER_LED;
+    colourMode = SLOW
+    def speedToogle():
+    def colourModeToogle():
+
+'''
+TASKS DEFINITION SECTION (3 TASKS)
+'''
+# Task for the colour sequence toogling button
+# Write:
+# - settings.colourMode
+def buttonColourModeIRQ():
+    settings.colourModeToggle()
+
+# Task for the speed toogling button
+# Write:
+# - settings.speed
+def buttonSpeedIRQ():
+    settings.speedToogle()
+
+# Task for the LEDs demonstration
+# Read:
+# - settings.colourMode to define which function to call
+#
+# ledPerLedDemo() and multipleLedsDemo()
+# Read:
+# - settings.speed to blink it faster or slower
+def ledDemoTask():
+    if settings.colourMode == LED_PER_LED:
+        ledPerLedDemo()
+    else:
+        multipleLedsDemo()
+
+def main():
+    # Instantiate a global Settings "class"
+    # Would be used to make the tasks communicate with each other
+    settings = Settings()
+
+    # Execute the 3 defined tasks
+    while(1):
+        ledDemoTask()
+        delay()
+
+main()
